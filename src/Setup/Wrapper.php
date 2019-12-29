e8f17048d4a15431e41d146072ba1479d3532cac
Shudd3r <q3.shudder@gmail.com>
2019-12-29 09:32:34 +0100

    Added phpDoc descriptions to Wrapper class

--- src/Setup/Wrapper.php
+++ src/Setup/Wrapper.php
@@ -14,6 +14,9 @@ namespace Polymorphine\Container\Setup;
 use Polymorphine\Container\Records\Record;
 
 
+/**
+ * ComposedInstanceRecord definition builder.
+ */
 class Wrapper
 {
     private $id;
@@ -28,6 +31,23 @@ class Wrapper
         $this->entry  = $entry;
     }
 
+    /**
+     * Defines class instantiation wrapping previously defined instance.
+     * This method is similar to Entry::instance() except it returns its
+     * own instance until Wrapper::compose() is called, and valid definition
+     * requires current entry identifier as one of given dependencies to
+     * inject wrapped object. Otherwise exception will be thrown.
+     *
+     * @see Entry::instance()
+     * @see Wrapper::compose()
+     *
+     * @param string $className
+     * @param string ...$dependencies
+     *
+     * @throws Exception\IntegrityConstraintException
+     *
+     * @return $this
+     */
     public function with(string $className, string ...$dependencies): self
     {
         $idx = array_search($this->id, $dependencies, true);
@@ -39,6 +59,10 @@ class Wrapper
         return $this;
     }
 
+    /**
+     * Adds ComposedInstanceRecord created with collected wrappers composition
+     * to container records.
+     */
     public function compose(): void
     {
         $this->entry->record(new Record\ComposedInstanceRecord($this->id, $this->record, $this->wrappers));
